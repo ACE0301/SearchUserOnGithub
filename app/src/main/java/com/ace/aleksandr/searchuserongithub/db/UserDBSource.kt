34fa913766973fd05c86a8d65c29ed204a58f41a
Aleksandr <efremov0301@mail.ru>
2019-04-28 23:18:30 +0300

    добавил примеры методов realm

---
+++ app/src/main/java/com/ace/aleksandr/searchuserongithub/db/UserDBSource.kt
@@ -0,0 +1,52 @@
+package com.ace.aleksandr.searchuserongithub.db
+
+import com.ace.aleksandr.searchuserongithub.CustomApplication
+import com.ace.aleksandr.searchuserongithub.model.RepoRealm
+import io.reactivex.Completable
+import io.reactivex.Single
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class UserDbSource {
+    private var mRealmConfiguration: RealmConfiguration? = null
+    val users: Single<List<RepoRealm>>
+        get() {
+            val realm = Realm.getInstance(mRealmConfiguration)
+            val users = realm.copyFromRealm(realm.where(RepoRealm::class.java).findAll())
+            realm.close()
+            return Single.just(users)
+        }
+
+    init {
+        //mRealmConfiguration = CustomApplication.getUserConfiguration()
+    }
+
+    fun getUser(id: Long): Single<RepoRealm> {
+        val realm = Realm.getInstance(mRealmConfiguration)
+        var user: RepoRealm? = null
+        val realmUser = realm.where(RepoRealm::class.java).equalTo("id", id).findFirst()
+        if (realmUser != null) {
+            user = realm.copyFromRealm(realmUser)
+        }
+        realm.close()
+        return Single.just(user)
+    }
+
+    fun updateUsers(users: List<RepoRealm>): Completable {
+        return Completable.fromAction {
+            val realm = Realm.getInstance(mRealmConfiguration)
+            realm.insertOrUpdate(users)
+            realm.close()
+            Realm.compactRealm(mRealmConfiguration)
+        }
+    }
+
+    fun updateUser(user: RepoRealm): Completable {
+        return Completable.fromAction {
+            val realm = Realm.getInstance(mRealmConfiguration)
+            realm.insertOrUpdate(user)
+            realm.close()
+            Realm.compactRealm(mRealmConfiguration)
+        }
+    }
+}
--- app/src/main/java/com/ace/aleksandr/searchuserongithub/view/usersbookmarks/BookmarksPresenter.kt
+++ app/src/main/java/com/ace/aleksandr/searchuserongithub/view/usersbookmarks/BookmarksPresenter.kt
@@ -1,26 +1,20 @@
 package com.ace.aleksandr.searchuserongithub.view.usersbookmarks
 
 import com.ace.aleksandr.searchuserongithub.base.BasePresenter
-import com.ace.aleksandr.searchuserongithub.data.api.ApiHolder
-import com.ace.aleksandr.searchuserongithub.view.usersearch.UserSearchView
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
+import com.ace.aleksandr.searchuserongithub.db.UserDbSource
+import io.realm.Realm
 
-class BookmarksPresenter(view: BookmarksView): BasePresenter<BookmarksView>(view) {
+class BookmarksPresenter(view: BookmarksView) : BasePresenter<BookmarksView>(view) {
     override fun onCreate() {
     }
 
-    private var disposableGetUserInfoBookmarks: Disposable? = null
 
     fun showUsersBookmarks() {
+        val realm = Realm.getDefaultInstance()
+        realm.where()
 
-        if (disposableGetUserInfoBookmarks != null && disposableGetUserInfoBookmarks?.isDisposed == false) {
-            disposableGetUserInfoBookmarks?.dispose()
-        }
     }
 
     override fun onDestroy() {
-        disposableGetUserInfoBookmarks?.dispose()
     }
 }
