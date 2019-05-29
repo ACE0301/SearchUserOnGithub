faec5cde77a14fb07767f2cdc5ff9d666a57bf99
Alexandr <efremov0301@mail.ru>
2019-05-29 20:57:54 +0300

    Изменил дизайн

---
+++ app/src/main/java/com/ace/aleksandr/searchuserongithub/model/user/impl/UserrepositoryImpl.kt
@@ -0,0 +1,10 @@
+package com.ace.aleksandr.searchuserongithub.model.user.impl
+
+import com.ace.aleksandr.searchuserongithub.model.user.repository.UserCloudDataSource
+
+class UserrepositoryImpl(
+    //private val userCloudDataSource: UserCloudDataSource = UserCloudDataSource()
+
+
+    ) {
+}
